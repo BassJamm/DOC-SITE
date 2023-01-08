d402d67538ed053660be6faad66176cf132a200e
ITJamm <75690476+ITJamm@users.noreply.github.com>
2023-01-08 10:55:11 +0000

    updates to blog

---
+++ build/assets/js/4675ac2b.f85a2434.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2258],{7605:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/blogpost","page":1,"postsPerPage":1,"totalPages":1,"totalCount":1,"blogDescription":"A tech blog powered by Docusaurus!","blogTitle":"IITJamms blog"}')}}]);
---
+++ build/assets/js/5f4bf7bb.03f4eff4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[6152],{8595:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/kb","page":1,"postsPerPage":1,"totalPages":1,"totalCount":1,"blogDescription":"A tech blog powered by Docusaurus!","blogTitle":"IITJamms blog"}')}}]);
